10911ed76aa14eb1c935c8ad1a2f1cc1d82fe4e8
Ashrit Mandava <ashritmandava@gmail.com>
2025-12-22 17:59:48 -0500

    Refactored getUI to getUserRequest, which handles input aswell

--- ServerApp.java
+++ ServerApp.java
@@ -1,17 +1,28 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+
 
 public class ServerApp {
-    public static String getUi(String id) {
+	private static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    public static String getUserRequest(String id) {
         switch (id) {
             case "selection-cipherType":
-                return "Select a cipher to use.\n\t1. Caesar Cipher\n\t2. Substitution Cipher\n\t3. Pair Shift Cipher\n\n";
+                System.out.println("Select a cipher to use.\n\t1. Caesar Cipher\n\t2. Substitution Cipher\n\t3. Pair Shift Cipher\n");
+				return scanner.nextLine();
+
+			case "input-cipherKey":
+                System.out.print("Enter the key to use: ");
+				return scanner.nextLine();
             
             case "selection-keyed-cipherAction":
-                return "Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\t3. Change the current key\n\n";
+                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\t3. Change the current key\n");
+				return scanner.nextLine();
 
             case "selection-unkeyed-cipherAction":
-                return "Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\n";
+                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n");
+				return scanner.nextLine();
 
             default:
                 return "UI Not Found";
@@ -19,25 +30,31 @@ public class ServerApp {
     }
 
 	public static void main(String[] args) {
-		try (ServerSocket serverSocket = new ServerSocket(5000)){
-			InetAddress localhost = InetAddress.getLocalHost();
-			System.out.println("[SERVER] IP address is " + localhost.getHostAddress().trim());
-	        System.out.println("[SERVER] Server started on port 5000.");
+		System.out.println(getUserRequest("selection-cipherType"));
 
-	        Socket socket = serverSocket.accept();
-	        System.out.println("[SERVER] Client 1 Connected");
 
-	        DataInputStream in = new DataInputStream(socket.getInputStream());
-	        String msg = "";
-
-	        while (!msg.equals("!~DISCONNECT~!")) {
-	            msg = in.readUTF();
-	            System.out.println("[CLIENT 1] " + msg);
-	        }
-
-	        System.out.println("[SERVER] Client 1 Connection Closed.");
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
 	}
+
+	// public static void main(String[] args) {
+	// 	try (ServerSocket serverSocket = new ServerSocket(5000)){
+	// 		InetAddress localhost = InetAddress.getLocalHost();
+	// 		System.out.println("[SERVER] IP address is " + localhost.getHostAddress().trim());
+	//         System.out.println("[SERVER] Server started on port 5000.");
+
+	//         Socket socket = serverSocket.accept();
+	//         System.out.println("[SERVER] Client 1 Connected");
+
+	//         DataInputStream in = new DataInputStream(socket.getInputStream());
+	//         String msg = "";
+
+	//         while (!msg.equals("!~DISCONNECT~!")) {
+	//             msg = in.readUTF();
+	//             System.out.println("[CLIENT 1] " + msg);
+	//         }
+
+	//         System.out.println("[SERVER] Client 1 Connection Closed.");
+	//     } catch (IOException e) {
+	//         e.printStackTrace();
+	//     }
+	// }
 }
