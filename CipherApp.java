74b376540b86931418c06e71016d8e3cc455231b
Ashrit Mandava <ashritmandava@gmail.com>
2025-12-22 18:37:41 -0500

    Basic Caesar Cipher Functionality

--- CipherApp.java
+++ CipherApp.java
@@ -3,25 +3,112 @@ import java.net.*;
 import java.util.Scanner;
 
 public class CipherApp {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.1.204", 5000);
-             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-             Scanner scanner = new Scanner(System.in)) {
+    private static Scanner scanner = new Scanner(System.in);
+    
+    /*
+    Multiple predefined user interface prompts identified by a unique string ID.
+    Returns user input as a string.
+    */
+    public static String getUserRequest(String id) {
+        switch (id) {
+            case "selection-cipherType":
+                System.out.println("Select a cipher to use.\n\t1. Caesar Cipher\n\t2. Substitution Cipher\n\t3. Pair Shift Cipher\n");
+				return scanner.nextLine();
 
-            System.out.println("[CLIENT] Connected to server.");
-            String message = "";
+			case "input-cipherKey":
+                System.out.print("Enter the key to use: ");
+				return scanner.nextLine();
+            
+            case "selection-keyed-cipherAction":
+                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\t3. Change the current key\n\t4. Return to main menu\n");
+				return scanner.nextLine();
 
-            while (!message.equals("!~DISCONNECT")) {
-                System.out.print("Enter message: ");
-                message = scanner.nextLine();
-                
-                out.writeUTF(message);
-                out.flush();
-            }
+            case "selection-unkeyed-cipherAction":
+                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\t3. Return to main menu\n");
+				return scanner.nextLine();
 
-            System.out.println("[CLIENT] Connection closed.");
-        } catch (IOException e) {
-            System.err.println("[CLIENT] Error: " + e.getMessage());
+            case "input-messageEncrypt":
+                System.out.print("Enter message to encrypt: ");
+				return scanner.nextLine();
+
+            case "input-messageDecrypt":
+                System.out.print("Enter message to decrypt: ");
+				return scanner.nextLine();
+
+            default:
+                System.out.println("UI Not Found");
+                return "UI Not Found";
         }
     }
+
+    public static void runCaesarCipher() {
+        // todo user 
+        String key = getUserRequest("input-cipherKey");
+        Cipherable caesarCipher = new CaesarCipher(Integer.parseInt(key));
+
+        String message = "";
+
+        String action = "0";
+
+        while (true) {
+            action = getUserRequest("selection-keyed-cipherAction");
+
+            switch (action) {
+                case "1":
+                    message = getUserRequest("input-messageEncrypt");
+                    String encodedMessage = caesarCipher.encode(message);
+                    System.out.println("Encoded Message: " + encodedMessage);
+                    break;
+                
+                case "2":
+                    message = getUserRequest("input-messageDecrypt");
+                    String decodedMessage = caesarCipher.decode(message);
+                    System.out.println("Decoded Message: " + decodedMessage);
+                    break;
+                
+                case "3":
+                    key = getUserRequest("input-cipherKey");
+                    caesarCipher = new CaesarCipher(Integer.parseInt(key));
+                    System.out.println("Key updated.");
+                    break;
+                
+                case "4":
+                    System.out.println("Returning to main menu.");
+                    break;
+
+                default:
+                    System.out.println("Invalid, please pick a valid option.");
+            }
+
+            if (action.equals("4")) {
+                break;
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+		runCaesarCipher();
+	}
+
+    // public static void main(String[] args) {
+    //     try (Socket socket = new Socket("192.168.1.204", 5000);
+    //          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+    //          Scanner scanner = new Scanner(System.in)) {
+
+    //         System.out.println("[CLIENT] Connected to server.");
+    //         String message = "";
+
+    //         while (!message.equals("!~DISCONNECT")) {
+    //             System.out.print("Enter message: ");
+    //             message = scanner.nextLine();
+                
+    //             out.writeUTF(message);
+    //             out.flush();
+    //         }
+
+    //         System.out.println("[CLIENT] Connection closed.");
+    //     } catch (IOException e) {
+    //         System.err.println("[CLIENT] Error: " + e.getMessage());
+    //     }
+    // }
 }
--- ServerApp.java
+++ ServerApp.java
@@ -4,37 +4,6 @@ import java.util.*;
 
 
 public class ServerApp {
-	private static java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-    public static String getUserRequest(String id) {
-        switch (id) {
-            case "selection-cipherType":
-                System.out.println("Select a cipher to use.\n\t1. Caesar Cipher\n\t2. Substitution Cipher\n\t3. Pair Shift Cipher\n");
-				return scanner.nextLine();
-
-			case "input-cipherKey":
-                System.out.print("Enter the key to use: ");
-				return scanner.nextLine();
-            
-            case "selection-keyed-cipherAction":
-                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n\t3. Change the current key\n");
-				return scanner.nextLine();
-
-            case "selection-unkeyed-cipherAction":
-                System.out.println("Select an action.\n\t1. Encode a message\n\t2. Decode a message\n");
-				return scanner.nextLine();
-
-            default:
-                return "UI Not Found";
-        }
-    }
-
-	public static void main(String[] args) {
-		System.out.println(getUserRequest("selection-cipherType"));
-
-
-	}
-
 	// public static void main(String[] args) {
 	// 	try (ServerSocket serverSocket = new ServerSocket(5000)){
 	// 		InetAddress localhost = InetAddress.getLocalHost();
